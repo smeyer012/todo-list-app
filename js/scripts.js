8d0fafe23f51567ecd650dd75fdcd2b656702f16
smeyer012 <115064549+smeyer012@users.noreply.github.com>
2022-11-09 20:34:23 -0600

    replaces JS with jQuery; improves UX of error message, input clearance, and enhanced styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,29 @@
 function newItem(){
 
-    //javascript
-    //1. Adding a new item to the list of items: 
-       let li = document.createElement("li");
-       let inputValue = document.getElementById("input").value;
-       let text = document.createTextNode(inputValue);
-       li.appendChild(text);
-    
-       if (inputValue === '') {
-         alert("You must write something!");
-       } else {
-         let list = document.querySelector('#list');
-         list.appendChild(li);
-       }
-    
-     //2. Crossing out an item from the list of items:
-       function crossOut() {
-             li.classList.toggle("strike");
-         }
-    
-         li.addEventListener("dblclick",crossOut);
-    
-     //3(i). Adding the delete button "X": 
-       let crossOutButton = document.createElement("crossOutButton");
-         crossOutButton.appendChild(document.createTextNode("X"));
-         li.appendChild(crossOutButton);
-    
-         crossOutButton.addEventListener("click", deleteListItem);
-     //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-       function deleteListItem(){
-             li.classList.add("delete")
-         }
-     // 4. Reordering the items: 
-       $('#list').sortable();
+        let inputValue = $('#input').val();
+
+        if (inputValue === '') {
+            $('#inputBox').addClass('error');
+            $('#inputBox').append('<label for="input">Type to-do item into the box above</label>');
+        } else {
+            $('#inputBox').removeClass('error');
+            $('#inputBox label').remove();
+            $('#list').append('<li>' + inputValue + '<span class="deleteItem">x</span></li>');
+            $('#input').val('');
+        }
+       
+        $('#list li').on("click", function() {
+            $(this).toggleClass("strike");
+        });
+       
+        $('.deleteItem').on("click", function() {
+            let $this = $(this);
+            $this.parent().css("opacity", 0);
+            setTimeout(function() { 
+                $this.parent().remove();
+            }, 600);
+        });
+     
+        //enables reordering 
+        $('#list').sortable();
 }
